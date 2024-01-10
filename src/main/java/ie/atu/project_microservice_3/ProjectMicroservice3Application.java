6d0604093c74aad10be5eb67cce4369c3d6288f7
Mikaela-D <G00401392@atu.ie>
2024-01-10 12:07:16 +0000

    Created new project Microservice 3

---
+++ src/main/java/ie/atu/project_microservice_3/ProjectMicroservice3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.project_microservice_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMicroservice3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMicroservice3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/project_microservice_3/ProjectMicroservice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.project_microservice_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMicroservice3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
