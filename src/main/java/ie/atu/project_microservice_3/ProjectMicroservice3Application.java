e1fe54d6cb6a529166c097d212b2e91433fb458b
Mikaela-D <G00401392@atu.ie>
2024-01-10 13:33:51 +0000

    Added new class TransactionDetails

--- src/main/java/ie/atu/project_microservice_3/ProjectMicroservice3Application.java
+++ src/main/java/ie/atu/project_microservice_3/ProjectMicroservice3Application.java
@@ -2,7 +2,10 @@ package ie.atu.project_microservice_3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProjectMicroservice3Application {
 
---
+++ src/main/java/ie/atu/project_microservice_3/TransactionDetails.java
@@ -0,0 +1,11 @@
+package ie.atu.project_microservice_3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDetails {
+}
