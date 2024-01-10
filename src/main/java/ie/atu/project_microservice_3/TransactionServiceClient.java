cddd6979993c1172f32639d4568c67ae802eb946
Mikaela-D <G00401392@atu.ie>
2024-01-10 13:46:48 +0000

    Added code to TransactionController

--- src/main/java/ie/atu/project_microservice_3/TransactionServiceClient.java
+++ src/main/java/ie/atu/project_microservice_3/TransactionServiceClient.java
@@ -1,4 +1,8 @@
 package ie.atu.project_microservice_3;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "confirmation-service", url = "http://localhost:8082")
 public interface TransactionServiceClient {
+
 }
