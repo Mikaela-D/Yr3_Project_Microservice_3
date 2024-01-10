f04b62c58c0ca046799a07b9474cd2eb46681e7a
Mikaela-D <G00401392@atu.ie>
2024-01-10 17:23:25 +0000

    Added code to TransactionService

--- src/main/java/ie/atu/project_microservice_3/TransactionService.java
+++ src/main/java/ie/atu/project_microservice_3/TransactionService.java
@@ -2,7 +2,25 @@ package ie.atu.project_microservice_3;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TransactionService {
+    private List<TransactionDetails> transactionList = new ArrayList<>();
 
+    public String rentMovie(TransactionDetails transactionDetails) {
+        transactionList.add(transactionDetails);
+        return "Movie rented successfully";
+    }
+
+    public String returnMovie(TransactionDetails transactionDetails) {
+        if (transactionList.removeIf(transaction ->
+                transaction.getUserID() == transactionDetails.getUserID()
+                        && transaction.getMovieID() == transactionDetails.getMovieID())) {
+            return "Movie returned successfully";
+        } else {
+            return "Movie wasn't found in the transaction list";
+        }
+    }
 }
